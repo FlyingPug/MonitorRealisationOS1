f2183f587c3261d5367423ec8753ea2e8d90243f
CrazyLlama <44662043+FlyingPug@users.noreply.github.com>
2023-12-01 21:55:17 +0700

    сделал независимость от класса монитора

--- MonitorRealisationOS1/MonitorRealisationOS1.cpp
+++ MonitorRealisationOS1/MonitorRealisationOS1.cpp
@@ -4,39 +4,67 @@
 #include <condition_variable>
 #include <string>
 
-class MessageMonitor {
+template <typename T>
+class Monitor {
 private:
-    std::string message;
-    bool isMessageAvailable;
+    T resource;
+    bool isResourceAvailable;
     std::mutex mutex;
     std::condition_variable condition;
+
+public:
+    Monitor() : isResourceAvailable(false) {}
+
+    void setResource(const T& newResource) {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            resource = newResource;
+            isResourceAvailable = true;
+        }
+        condition.notify_one();
+    }
+
+    T getResource() {
+        std::unique_lock<std::mutex> lock(mutex);
+        condition.wait(lock, [this]() { return isResourceAvailable; });
+
+        isResourceAvailable = false;
+        return resource;
+    }
+};
+
+class MessageProducer {
+private:
+    Monitor<std::string>& monitor;
     int messageNum;
 
 public:
-    MessageMonitor() : isMessageAvailable(false), messageNum(0) {}
+    MessageProducer(Monitor<std::string>& monitor) : monitor(monitor), messageNum(0) {}
 
     void produceMessage() {
         while (true) {
             std::this_thread::sleep_for(std::chrono::seconds(1));
 
-            {
-                std::lock_guard<std::mutex> lock(mutex);
-                message = "Сообщение " + std::to_string(messageNum);
-                std::cout << "Отправлено: " << message << std::endl;
-                isMessageAvailable = true;
-                messageNum++;
-                condition.notify_one();
-            }
+            std::string message = "Сообщение " + std::to_string(messageNum);
+            std::cout << "Отправлено: " << message << std::endl;
+
+            monitor.setResource(message);
+            messageNum++;
         }
     }
+};
+
+class MessageConsumer {
+private:
+    Monitor<std::string>& monitor;
+
+public:
+    MessageConsumer(Monitor<std::string>& monitor) : monitor(monitor) {}
 
     void consumeMessage() {
         while (true) {
-            std::unique_lock<std::mutex> lock(mutex);
-            condition.wait(lock, [this]() { return isMessageAvailable; });
-
+            std::string message = monitor.getResource();
             std::cout << "Обработано: " << message << std::endl;
-            isMessageAvailable = false;
         }
     }
 };
@@ -44,10 +72,12 @@ public:
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    MessageMonitor messageMonitor;
+    Monitor<std::string> messageMonitor;
+    MessageProducer producer(messageMonitor);
+    MessageConsumer consumer(messageMonitor);
 
-    std::thread producerThread(&MessageMonitor::produceMessage, &messageMonitor);
-    std::thread consumerThread(&MessageMonitor::consumeMessage, &messageMonitor);
+    std::thread producerThread(&MessageProducer::produceMessage, &producer);
+    std::thread consumerThread(&MessageConsumer::consumeMessage, &consumer);
 
     producerThread.join();
     consumerThread.join();
